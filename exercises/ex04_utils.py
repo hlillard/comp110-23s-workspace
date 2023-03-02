d620fe3b3130e4df1e56060413c2b255d52f2f08
Hannah Lillard <hlillard@unc.edu>
2023-03-01 23:35:21 -0500

    ex045

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,9 +2,8 @@
 
 __author__ = "730476613"
 
-
 def all(list: list[int], int) -> bool:
-    """Determine whether each element in list of ints is equal to given int."""
+    # determine whether each element in list of ints is equal to given int
     if not list:
         return False
     while list:
@@ -13,9 +12,8 @@ def all(list: list[int], int) -> bool:
             return False
     return True
 
-
 def max(input: list[int]) -> int:
-    """Find max integer in given list."""
+    # find max integer in given list
     if len(input) == 0:
         raise ValueError("max() arg is an empty sequence")
     current_max = input[0]
@@ -24,9 +22,8 @@ def max(input: list[int]) -> int:
             current_max = input[int]
     return current_max
 
-
 def is_equal(list1: list[int], list2: list[int]) -> bool:
-    """Do 2 lists have the same length?""""
+    # do 2 lists have the same length?
     if len(list1) != len(list2):
         return False
     for int in range(len(list1)):
